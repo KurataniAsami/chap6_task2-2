3d396275069c174488d7442f9c0d027f2b87999d
kurataniasami <kura56135613@gmail.com>
2025-12-02 15:48:33 +0900

    変更

--- src/App.js
+++ src/App.js
@@ -1,19 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import BlogList from './Home/BlogList';
-import PostDetail from './detail/PostDetail';
-import { posts } from './data/Post';
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<BlogList posts={posts} />} />
-
-        <Route path="/post/:id" element={<PostDetail posts={posts} />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
 // 
 
 
